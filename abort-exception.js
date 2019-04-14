2e413d50f027df114d936cda40bb8d478bea0f20
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-04-14 16:49:40 -0400

    Initial commit.

---
+++ abort-exception.js
@@ -0,0 +1,46 @@
+"use module"
+
+const
+  nativeAbortException= typeof AbortException!== "undefined"&& AbortException,
+  domException= typeof DOMException!== "undefined",
+  origBaseError= domException? DOMException: Error
+let baseError= origBaseError
+
+export function generateAbortException( baseErrorClass= baseError){
+	if( nativeAbortException){
+		return nativeAbortException
+	}
+	const abortException= (function(){
+		// we use the IIFE so this class-expression can implicitly get the desired `name`.
+		const AbortException= class extends baseErrorClass{}
+		return AbortException
+	})()
+	return abortException
+}
+
+export function setAbortException( newAbortException, doForce){
+	const origAbortException= abortException
+	if( !doForce&& nativeAbortException){
+		throw new Error("Already have a native AbortException")
+	}
+	abortException= newAbortException
+	return origAbortException
+}
+
+export function setBaseError( newBaseError= origBaseError){
+	if( newBaseError=== false){
+		newBaseError= undefined
+	}
+	if( newBaseError=== baseError){
+		return abortException
+	}
+	const generated= generateAbortException( newBaseError)
+	setAbortException( generated)
+	return generated
+}
+
+let abortException= generateAbortException()
+export {
+  abortException as AbortException,
+  abortException as default
+}
